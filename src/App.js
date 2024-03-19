607a99827b8d6d743f57f0616211ab3582773b66
Eren <erenalparslan52@gmail.com>
2024-03-19 23:51:12 +0300

    sidebar removed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Sidebar from './Dashboard/Dashboard';
 import Login from './Login/Login';
 
 function App() {
-  const menuItems = [
-    { title: 'Anasayfa', url: '#' },
-    { title: 'Hakkımızda', url: '#' },
-    { title: 'Hizmetler', url: '#' },
-    { title: 'İletişim', url: '#' },
-];
   return (
     <div className="App">
-      <Sidebar items={menuItems}/>
+  
       <Login/>
    
     </div>
